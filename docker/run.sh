4899fc10a9ea82ae7efad3bdc65937d21e71869b
rmpestano <bigboss666>
2016-03-26 02:53:46 -0300

    added docker section to readme

--- docker/run.sh
+++ docker/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --name jenkins-living-documentation -p 8080:8080 -v /var/jenkins_home rmpestano/jenkins-living-documentation
+docker run -it --name jenkins-living-docs -p 8080:8080 -v /var/jenkins_home rmpestano/jenkins-living-documentation
